62f94d24b49e10ecbbe5e8015d21eac7e3814ebb
Jorri Fransen <jorri@jorrifransen.nl>
2023-10-01 17:47:55 +0200

    Basic lsp and autocomplete setup.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,7 +7,6 @@ vim.wo.number = true
 vim.o.relativenumber = true
 vim.wo.relativenumber = true
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -43,6 +42,7 @@ require('lazy').setup({
             vim.g.vimspector_enable_mappings = "HUMAN"
         end,
         cmd = { 'VimspectorInstall', 'VimspectorUpdate' },
+        ---@diagnostic disable: missing-fields
 	keys = {
             { "<F5>" },
             { "<F6>" },
@@ -96,9 +96,6 @@ require('lazy').setup({
         },
     },
 
-    -- Useful plugin to show you pending keybinds.
-    { 'folke/which-key.nvim', opts = {} },
-
     -- Themes
     'rebelot/kanagawa.nvim',
     'EdenEast/nightfox.nvim',
@@ -122,6 +119,28 @@ require('lazy').setup({
         main = "ibl",
         opts = {},
     },
+
+    -- Lsp stuff
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            { 'williamboman/mason.nvim', config = true },
+            'williamboman/mason-lspconfig.nvim',
+
+            -- Useful status updates for LSP
+            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+            { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+
+            'folke/neodev.nvim',
+        },
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            -- Adds LSP completion capabilities
+            'hrsh7th/cmp-nvim-lsp',
+        }
+    }
 })
 
 
@@ -210,8 +229,8 @@ require('telescope').setup {
 
 pcall(require('telescope').load_extension, 'fzf')
 
-telescope_fn = require('telescope.builtin')
-telescope_dropdown = require('telescope.themes').get_dropdown { winblend = 10, previewer = false }
+local telescope_fn = require('telescope.builtin')
+local telescope_dropdown = require('telescope.themes').get_dropdown { winblend = 10, previewer = false }
 
 vim.keymap.set('n', '<leader>ff', telescope_fn.find_files, { noremap = true })
 vim.keymap.set('n', '<leader>bb', function () telescope_fn.buffers(telescope_dropdown) end, { noremap = true })
@@ -312,7 +331,7 @@ ltab.setup({
         local buftype = vim.fn.getbufvar(bufnr, '&buftype')
         local filetype = vim.fn.getbufvar(bufnr, '&filetype')
 
-      if buftype == 'help' then
+        if buftype == 'help' then
             return 'help:' .. vim.fn.fnamemodify(file, ':t:r')
         elseif buftype == 'quickfix' then
             return 'quickfix'
@@ -373,4 +392,115 @@ require('neo-tree').setup {
             hide_gitignored = true,
         }
     }
+}
+
+-- [[ Configure LSP ]]
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+    -- NOTE: Remember that lua is a real programming language, and as such it is possible
+    -- to define small helper and utility functions so you don't have to repeat yourself
+    -- many times.
+    --
+    -- In this case, we create a function that lets us more easily define mappings specific
+    -- for LSP related items. It sets the mode, buffer and description for us each time.
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+    nmap('gd', vim.lsp.buf.definition, "Goto definition")
+end
+--
+-- Enable the following language servers
+--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+--
+--  Add any additional override configuration in the following tables. They will be passed to
+--  the `settings` field of the server config. You must look up that documentation yourself.
+--
+--  If you want to override the default filetypes that your language server will attach to you can
+--  define the property 'filetypes' to the map in question.
+local servers = {
+    clangd = {},
+    -- gopls = {},
+    -- pyright = {},
+    -- rust_analyzer = {},
+    -- tsserver = {},
+    -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+            diagnostics = {
+                -- disable = { 'missing-fields' },
+            }
+        },
+    },
+}
+
+-- Setup neovim lua configuration
+require('neodev').setup()
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+    ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+    function(server_name)
+        require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+            filetypes = (servers[server_name] or {}).filetypes,
+        }
+    end
+}
+
+-- [[ Configure nvim-cmp ]]
+-- See `:help cmp`
+local cmp = require 'cmp'
+
+---@diagnostic disable: missing-fields
+cmp.setup {
+    mapping = cmp.mapping.preset.insert {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete {},
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+    },
 }
