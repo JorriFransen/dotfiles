8829a1847d45dd01d48112bf1d99426d71d11fc7
Jorri Fransen <jorri@jorrifransen.nl>
2024-02-20 01:09:20 +0100

    Lsp cleanup.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -206,37 +206,14 @@ require('lazy').setup({
     { 'rhysd/vim-llvm', },
 
     -- Lsp stuff
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            { 'williamboman/mason.nvim', config = true },
-            'williamboman/mason-lspconfig.nvim',
-
-            'folke/neodev.nvim',
-
-        },
-    },
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            -- Snippet Engine & its associated nvim-cmp source
-            -- 'L3MON4D3/LuaSnip',
-            -- 'saadparwaiz1/cmp_luasnip',
-
-            -- Adds LSP completion capabilities
-            'hrsh7th/cmp-nvim-lsp',
-            -- 'hrsh7th/cmp-nvim-lsp-signature-help',
-            -- {
-            --     'ray-x/lsp_signature.nvim',
-            --     event = "VeryLazy",
-            --     opts = {},
-            --     config = function(_, opts) require'lsp_signature'.setup(opts) end,
-            -- },
-
-            -- Adds a number of user-friendly snippets
-            -- 'rafamadriz/friendly-snippets',
-        }
-    }
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "folke/neodev.nvim" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "L3MON4D3/LuaSnip" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "neovim/nvim-lspconfig" },
 })
 
 vim.o.cursorline = true
@@ -730,7 +707,41 @@ local function nui_lsp_rename()
   input:on(event.BufLeave, input.input_props.on_close, { once = true })
 end
 
--- [[ Configure LSP ]]
+vim.keymap.set('n', '<leader>ga', function() vim.cmd('ClangdSwitchSourceHeader') end);
+vim.keymap.set('n', '<F1>', function() Compile() end)
+vim.keymap.set('i', '<F1>', function() vim.cmd('stopinsert') Compile() end)
+vim.keymap.set('n', '<leader><F1>', function() EmitCompileCommands() end)
+vim.keymap.set('n', '<F2>', function() Clean() end)
+vim.keymap.set('i', '<F2>', function() vim.cmd('stopinsert') Clean() end)
+vim.keymap.set('n', '<F3>', function() Iwyu() end)
+vim.keymap.set('n', '<leader>rr', function() Run() end)
+vim.keymap.set('n', '<leader>rt', function() RunTests() end)
+vim.keymap.set('n', '<leader>ror', function() RunSetOptions() end)
+vim.keymap.set('n', '<leader>rot', function() RunTestsSetOptions() end)
+vim.keymap.set('n', '<leader>sbd', function() SetBuildDir() end)
+
+
+local servers = {
+    clangd = {},
+    bashls = {},
+    vimls = {},
+    lua_ls = {},
+}
+
+require("mason").setup()
+local mason_lspconfig = require("mason-lspconfig")
+mason_lspconfig.setup({
+    ensure_installed = vim.tbl_keys(servers),
+})
+
+require("neodev").setup()
+
+local lspconfig = require("lspconfig")
+
+local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+local luasnip = require("luasnip")
+
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
     -- NOTE: Remember that lua is a real programming language, and as such it is possible
@@ -765,67 +776,10 @@ local on_attach = function(_, bufnr)
     nmap('K', vim.lsp.buf.hover, "Hover documentation")
     nmap('<C-s>', vim.lsp.buf.signature_help, "Signature documentation")
 end
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
-local servers = {
-    clangd = {},
-    bashls = {},
-    vimls = {},
-    -- gopls = {},
-    -- pyright = {},
-    -- rust_analyzer = {},
-    -- tsserver = {},
-    -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
-    lua_ls = {
-        Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-            diagnostics = {
-                -- disable = { 'missing-fields' },
-            }
-        },
-    },
-}
-
-vim.keymap.set('n', '<leader>ga', function() vim.cmd('ClangdSwitchSourceHeader') end);
-vim.keymap.set('n', '<F1>', function() Compile() end)
-vim.keymap.set('i', '<F1>', function() vim.cmd('stopinsert') Compile() end)
-vim.keymap.set('n', '<leader><F1>', function() EmitCompileCommands() end)
-vim.keymap.set('n', '<F2>', function() Clean() end)
-vim.keymap.set('i', '<F2>', function() vim.cmd('stopinsert') Clean() end)
-vim.keymap.set('n', '<F3>', function() Iwyu() end)
-vim.keymap.set('n', '<leader>rr', function() Run() end)
-vim.keymap.set('n', '<leader>rt', function() RunTests() end)
-vim.keymap.set('n', '<leader>ror', function() RunSetOptions() end)
-vim.keymap.set('n', '<leader>rot', function() RunTestsSetOptions() end)
-vim.keymap.set('n', '<leader>sbd', function() SetBuildDir() end)
-
--- Setup neovim lua configuration
-require('neodev').setup()
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Ensure the servers above
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-    ensure_installed = vim.tbl_keys(servers),
-}
---
 mason_lspconfig.setup_handlers {
     function(server_name)
-        require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
+        lspconfig[server_name].setup {
+            capabilities = lsp_capabilities,
             on_attach = on_attach,
             settings = servers[server_name],
             filetypes = (servers[server_name] or {}).filetypes,
@@ -833,66 +787,35 @@ mason_lspconfig.setup_handlers {
     end
 }
 
-
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
--- local luasnip = require 'luasnip'
--- require('luasnip.loaders.from_vscode').lazy_load();
--- luasnip.config.setup {}
-
----@diagnostic disable: missing-fields
-cmp.setup {
-    -- snippet = {
-    --     expand = function(args)
-    --         luasnip.lsp_expand(args.body)
-    --     end
-    -- },
-    mapping = cmp.mapping.preset.insert {
-        -- ['<Tab>'] = cmp.mapping.select_next_item(),
-        -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            -- elseif luasnip.expand_or_locally_jumpable() then
-            --     luasnip.expand_or_jump()
-           else
-                fallback()
-            end
-        end, { 'i', 's' }),
-
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            -- elseif luasnip.locally_jumpable(-1) then
-            --     luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
+local cmp = require("cmp") -- nvim-cmp
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
     },
     sources = {
-        { name = 'nvim_lsp' },
-        -- { name = 'nvim_lsp_signature_help' },
-        -- { name = 'luasnip' },
+        { name = "nvim_lsp"},
+        { name = "buffer" },
+
     },
-}
 
--- Insert '(' after selecting a function
--- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
--- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4), -- Up
+        ['<C-f>'] = cmp.mapping.scroll_docs(4), -- Down
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<S-Tab'] = cmp.mapping.select_prev_item(),
+        -- ['<CR>'] = cmp.mapping.complete(), -- doesn't seem to do anything?
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+    }),
 
-local ft =require('Comment.ft')
+})
+
+local ft = require('Comment.ft')
 ft.set('zc', { '//%s', '/*%s/*'})
 
