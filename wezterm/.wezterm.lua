7051ca2e9f9716faa120736305e430703277cdc3
Jorri Fransen <jorrifransen@gmail.com>
2024-12-12 22:33:23 +0100

    WEZTERM use X11 since wayland fullscreen is broken (and edges don't match screen edges).

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,21 +1,19 @@
-
--- local wezterm = require 'wezterm'
-
 local config = wezterm.config_builder();
 
-config.color_scheme = 'tokyonight_night'
-config.colors = { background = '#1a1b26' }
-config.window_background_opacity = 0.95
--- config.font = wezterm.font('Hack')
+return {
+    color_scheme = 'tokyonight_night',
+    colors = { background = '#1a1b26' },
+    window_background_opacity = 0.95,
+    -- font = wezterm.font('Hack'),
 
-config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-config.freetype_load_flags = "NO_HINTING"
--- config.freetype_load_flags = "DEFAULT"
-config.freetype_load_target = "Light"
--- config.freetype_load_target = "HorizontalLcd"
-config.freetype_render_target = "HorizontalLcd"
+    use_fancy_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
+    freetype_load_flags = "NO_HINTING",
+    -- freetype_load_flags = "DEFAULT",
+    freetype_load_target = "Light",
+    -- freetype_load_target = "HorizontalLcd"
+    freetype_render_target = "HorizontalLcd",
 
-
-
-return config
+    -- This makes fullscreen work on wayland...
+    enable_wayland = false
+}
