296e8cfc71856028819b24315a72e6a7bb816a1e
Jorri Fransen <jorrifransen@gmail.com>
2024-12-12 22:34:32 +0100

    NVIM updated vulkan-test local config.

--- nvim/.config/nvim/configs/vulkan-test.lua
+++ nvim/.config/nvim/configs/vulkan-test.lua
@@ -5,28 +5,39 @@ local function cmd(_cmd)
 end
 
 local function in_map(key, fn)
-   vim.keymap.set('i', key, fn);
-   vim.keymap.set('n', key, fn);
+   vim.keymap.set('i', key, fn)
+   vim.keymap.set('n', key, fn)
 end
 local function n_map(key, fn)
-   vim.keymap.set('n', key, fn);
+   vim.keymap.set('n', key, fn)
 end
 
-local common_options = " "
-function Default_Common_Options() common_options = " -Dglfw_window_api=x11 " end
-function Set_Common_Options()
-   vim.ui.input({prompt = "common_options=", default=common_options},
-                  function(input) common_options = input end)
+local build_options = " "
+function Default_Build_Options() build_options = " -Dvulkan_verbose=true " end
+function Set_Build_Options()
+   vim.ui.input({prompt = "build_options=", default=string.sub(build_options, 2, string.len(build_options) - 1)},
+      function(input) if input then build_options = " " .. input .. " " end end)
 end
+Default_Build_Options()
 
-Default_Common_Options()
+local run_options = " "
+function Default_Run_Options() run_options = " --glfw-window-api x11 " end
+function Set_Run_Options()
+   vim.ui.input({prompt = "run_options=", default=string.sub(run_options, 2, string.len(run_options) - 1)},
+      function(input) if input then run_options = " " .. input .. " " end end)
+end
+Default_Run_Options()
 
-in_map('<F1>', function() cmd("zig build" .. common_options .. " run") end);
-in_map('<F2>', function() cmd("zig build" .. common_options .. " clean") end);
--- in_map('<F3>', Build_And_Test);
-in_map('<F4>', function() vim.cmd('AsyncStop') end);
-n_map('<leader>cbe', Set_Common_Options);
-n_map('<leader>cbd', Default_Common_Options);
+in_map('<F1>', function() cmd("zig build" .. build_options .. "run --" .. run_options) end)
+in_map('<F2>', function() cmd("zig build" .. build_options .. "clean") end)
+-- in_map('<F3>', Build_And_Test)
+in_map('<F4>', function() vim.cmd('AsyncStop') end)
 
--- vim.api.nvim_create_autocmd("BufWritePost", { pattern = '*.zig', command = 'silent !zig fmt <afile>'})
+n_map('<leader>sbo', Set_Build_Options)
+n_map('<leader>dbo', Default_Build_Options)
+
+n_map('<leader>sro', Set_Run_Options)
+n_map('<leader>dro', Default_Run_Options)
+
+vim.api.nvim_create_autocmd("BufWritePost", { pattern = '*.zig', command = 'silent !zig fmt <afile>'})
 
