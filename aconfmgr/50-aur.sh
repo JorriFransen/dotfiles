d195fd1378983382ae1fa5f8e82a8e9e04fd3db6
Jorri Fransen <jorrifransen@gmail.com>
2025-01-04 00:13:41 +0100

    slimbook arch aconfig.

---
+++ aconfmgr/50-aur.sh
@@ -0,0 +1,12 @@
+
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+AddPackage --foreign brave-bin # Web browser that blocks ads and trackers by default (binary release)
+AddPackage --foreign gitkraken # The intuitive, fast, and beautiful cross-platform Git client.
+AddPackage --foreign ivpn # IVPN - Secure VPN for Privacy (CLI)
+AddPackage --foreign ivpn-ui # IVPN - Secure VPN for Privacy
+AddPackage --foreign paru-bin # Feature packed AUR helper
+AddPackage --foreign paru-bin-debug # Detached debugging symbols for paru-bin
+AddPackage --foreign tor-browser-bin # Tor Browser Bundle: anonymous browsing using Firefox and Tor
+AddPackage --foreign yubico-authenticator # Yubico Authenticator for Desktop
+AddPackage --foreign zig-dev-bin # A general-purpose programming language and toolchain for maintaining robust, optimal, and reusable software
+AddPackage --foreign zls-git # Zig LSP implementation + Zig Language Server
