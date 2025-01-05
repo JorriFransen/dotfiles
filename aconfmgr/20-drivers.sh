c35a5db771411baaa0a75e7910d2e7da3d229e3e
Jorri Fransen <jorrifransen@gmail.com>
2025-01-05 17:33:41 +0100

    ACONFMGR Switching to closed nvidia driver.

--- aconfmgr/20-drivers.sh
+++ aconfmgr/20-drivers.sh
@@ -1,5 +1,6 @@
 
-AddPackage nvidia-open # NVIDIA open kernel modules
+# AddPackage nvidia-open # NVIDIA open kernel modules
+AddPackage nvidia # NVIDIA kernel modules
 AddPackage nvidia-prime # NVIDIA Prime Render Offload configuration and utilities
 AddPackage xf86-video-amdgpu # X.org amdgpu video driver
 
