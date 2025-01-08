957b1a1e41e7c6a094fdb6c14aa96475561036ff
Jorri Fransen <jorrifransen@gmail.com>
2025-01-08 17:45:22 +0100

    ACONFMGR nvidia-open again.

--- aconfmgr/20-drivers.sh
+++ aconfmgr/20-drivers.sh
@@ -1,5 +1,5 @@
 
-AddPackage nvidia # NVIDIA kernel modules
+AddPackage nvidia-open # NVIDIA kernel modules
 AddPackage nvidia-prime # NVIDIA Prime Render Offload configuration and utilities
 AddPackage nvidia-settings # Tool for configuring the NVIDIA graphics driver
 
