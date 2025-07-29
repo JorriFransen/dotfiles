1b29c8c6b5faa771aba16596da4460444a1187fe
Jorri Fransen <jorrifransen@gmail.com>
2025-07-30 01:35:36 +0200

    Nvim update lsp scroll doc bindings.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -460,7 +460,7 @@ cmp.setup({
     },
     mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        -- ["<C-f>"] = cmp.mapping.scroll_docs(4), -- conflicts with tmux-sessionizer
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
         ["<C-space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({select = true}),
